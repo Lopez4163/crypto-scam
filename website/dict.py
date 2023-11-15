2fa97c52463dfd194b5c7f060e30a31a874130d7
Nicholas Lopez <Lopez4163@gmail.com>
2023-11-15 13:15:28 -0600

    working env

--- website/dict.py
+++ website/dict.py
@@ -1,61 +1,48 @@
-from calendar import c
-from cgi import print_exception
-from cgitb import html
-from itertools import combinations_with_replacement
-from operator import attrgetter
-from os import remove
-from xml.dom.minidom import Attr
 from flask import render_template
-import re
 from bs4 import BeautifulSoup
 import requests
-import time 
-from datetime import datetime, timedelta
-from threading import Timer
 
-
-url = f"https://coinmarketcap.com/"
+url = "https://coinmarketcap.com/"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
 tbody = doc.tbody
 trs = tbody.contents
-#RAW DICTS  
+
+# RAW DICTS
 crypto_coins = {}
 ex_crypto = {}
 
-#FIRST 10 COIN NAMES AND PRICES
-for tr in trs[:10]: #only first 10 rows, rest are not under p tag thats why they dont show
+# FIRST 10 COIN NAMES AND PRICES
+for tr in trs[:10]:
     name, price = tr.contents[2:4]
     names = name.p.string
     prices = price.a.string
     crypto_coins[names] = prices
 
-
-#REST OF NAMES AND PRICES 
+# REST OF NAMES AND PRICES 
 for tr in trs[10:]:
     name_ext, price_ext = tr.contents[2:4]
-    name_ex = name_ext.span.next.string 
-    price_ex = str(price_ext.span).split("<")[-2].split(">")[1]
+
+    # Check if span exists in price_ext
+    if price_ext.span:
+        # Extract the price if the span exists
+        name_ex = name_ext.span.next.string 
+        price_ex = str(price_ext.span).split("<")[-2].split(">")[1]
+    else:
+        # Handle the case where span does not exist
+        name_ex = "N/A"
+        price_ex = "N/A"
+
     ex_crypto[name_ex] = price_ex
-    
+
 for key in ex_crypto:
     ex_crypto[key] = '$' + ex_crypto[key]
-    
-    
-def Merge(crypto_coins, ex_crypto):
+
+def merge(crypto_coins, ex_crypto):
     res = crypto_coins | ex_crypto
     return res
 
-coin_dict = Merge(crypto_coins, ex_crypto)
+coin_dict = merge(crypto_coins, ex_crypto)
 
-
-
-
-
-
-
-
-
-
-
+# Rest of your code...
